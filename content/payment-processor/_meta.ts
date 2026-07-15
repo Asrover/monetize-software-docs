c45690e1bda5a82cbc349ea20a5456c187587443
aleksandr1strel <aleksandr1strel@gmail.com>
2026-07-15 12:29:56 +0300

    upd

--- content/payment-processor/_meta.ts
+++ content/payment-processor/_meta.ts
@@ -1,5 +1,6 @@
 export default {
   overview: 'Overview',
+  'sandbox-processors': 'Test with our sandbox processors',
   'create-payment-processor': 'Create payment processor',
   'connect-payment-processor': 'Connect payment processor to paywall'
 };
